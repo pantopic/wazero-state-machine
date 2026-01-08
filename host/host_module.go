f5fcbbfb33d3fa1aa691b2143118849b2599d8af
kevburnsjr <kevburnsjr@gmail.com>
2026-01-08 07:47:03 -0800

    Use canonical copy argument order

--- host/host_module.go
+++ host/host_module.go
@@ -85,7 +85,7 @@ func (p *hostModule) InitContext(ctx context.Context, m api.Module) (context.Con
 }
 
 // ContextCopy populates dst context with the meta page from src context.
-func (h *hostModule) ContextCopy(src, dst context.Context) context.Context {
+func (h *hostModule) ContextCopy(dst, src context.Context) context.Context {
 	dst = context.WithValue(dst, h.ctxKeyMeta, get[*meta](src, h.ctxKeyMeta))
 	return dst
 }
